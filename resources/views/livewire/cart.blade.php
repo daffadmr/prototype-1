17d53ad0280d023dc153326ba439ab6f5b16199c
daffadmr <daffaardhika@gmail.com>
2021-06-10 20:54:43 +0700

    edit view cart

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,15 +1,123 @@
 <div>
     <section class="cart my-5">
-        <div class="w-full">
-            <div class="py-3 mb-4 text-black text-sm">
-                <ul class="flex">
-                    <li><a href="{{ route('home') }}" class="underline">Home</a></li>
-                    <li><span class="mx-2">/</span></li>
-                    <li class="font-bold">Cart</li>
-                </ul>
-            </div>
-        </div>
+        <div class="flex justify-center my-6">
+            <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+                <div class="flex-1">
+                    <table class="w-full text-sm lg:text-base" cellspacing="0">
+                        <thead>
+                            <tr class="h-12 uppercase">
+                                <th class="hidden md:table-cell"></th>
+                                <th class="text-left">Product</th>
+                                <th class="lg:text-right text-left pl-5 lg:pl-0">
+                                    <span class="lg:hidden" title="Quantity">Qtd</span>
+                                    <span class="hidden lg:inline">Quantity</span>
+                                </th>
+                                <th class="hidden text-right md:table-cell">Unit price</th>
+                                <th class="text-right">Total price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="hidden pb-4 md:table-cell">
+                                    <a href="#">
+                                        <img src="https://limg.app/i/Calm-Cormorant-Catholic-Pinball-Blaster-yM4oub.jpeg"
+                                            class="w-20 rounded" alt="Thumbnail">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <p class="mb-2 md:ml-4">Earphone</p>
+                                        <form action="" method="POST">
+                                            <button type="submit" class="text-gray-700 md:ml-4">
+                                                <small>(Remove item)</small>
+                                            </button>
+                                        </form>
+                                    </a>
+                                </td>
+                                <td class="text-right">
+                                    <span class="text-sm lg:text-base font-medium">
+                                        20.00€
+                                    </span>
+                                </td>
+                                <td class="hidden text-right md:table-cell">
+                                    <span class="text-sm lg:text-base font-medium">
+                                        10.00€
+                                    </span>
+                                </td>
+                                <td class="text-right">
+                                    <span class="text-sm lg:text-base font-medium">
+                                        20.00€
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <hr class="pb-6 mt-6">
+                    <div class="my-4 mt-6 -mx-2 lg:flex">
+                        <div class="lg:px-2 lg:w-1/2">
+                            <div class="p-4 bg-gray-100 rounded-full">
+                                <h1 class="ml-2 font-bold uppercase">Jasa Pengiriman</h1>
+                            </div>
+                            <div>
+                                <div class="mt-6 mb-6">
+                                    <button class="flex items-center justify-between w-full bg-white rounded-md border-2 focus:border-blue-500 p-4 focus:outline-none">
+                                        <label class="flex items-center">
+                                            <input type="radio" name="pengantaran" class="form-radio border h-5 w-5 text-blue-600"><span class="ml-2 text-sm text-gray-700">SiCepat</span>
+                                        </label>
+                                        <span class="text-gray-600 text-sm">Rp.10,000.</span>
+                                    </button>
+                                    <button class="mt-6 flex items-center justify-between w-full bg-white rounded-md border-2 focus:border-blue-500 p-4 focus:outline-none">
+                                        <label class="flex items-center">
+                                            <input type="radio" name="pengantaran" class="form-radio border h-5 w-5 text-blue-600"><span class="ml-2 text-sm text-gray-700">AntarAja</span>
+                                        </label>
+                                        <span class="text-gray-600 text-sm">Rp.10,000.</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lg:px-2 lg:w-1/2">
+                            <div class="p-4 bg-gray-100 rounded-full">
+                                <h1 class="ml-2 font-bold uppercase">Order Details</h1>
+                            </div>
+                            <div class="p-4">
+                                <div class="flex justify-between border-b">
+                                    <div
+                                        class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                                        Barang
+                                    </div>
+                                    <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                                        Rp. 200,000.
+                                    </div>
+                                </div>
+                                <div class="flex justify-between border-b">
+                                    <div
+                                        class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                                        Ongkos Kirim
+                                    </div>
+                                    <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                                        Rp. 10,000.
+                                    </div>
+                                </div>
+                                <div class="flex justify-between pt-4 border-b">
+                                    <div
+                                        class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
+                                        Total
+                                    </div>
+                                    <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
+                                        Rp. 210,000.
+                                    </div>
+                                </div>
+                                <a href="#">
+                                    <button
+                                        class="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
+                                        <span class="ml-2 mt-5px"><i class="fas fa-credit-card"></i> Procceed to checkout</span>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
 
-        <p class="text-3xl mt-4 font-bold">Cart</p>
+                    </div>
+                </div>
+            </div>
     </section>
 </div>
